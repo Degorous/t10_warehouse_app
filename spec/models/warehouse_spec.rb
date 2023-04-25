617776bc39832c18735173a52f1b5b5515f29421
Erick Palombo <erick_palombo_@hotmail.com>
2023-04-25 18:47:55 -0300

    primeiro commit

--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -1,4 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe Warehouse, type: :model do
+  describe '#valid?' do
+    context 'presence' do
+      it 'false when name is empty' do
+        #Arrange
+        warehouse = Warehouse.new(name: '', code: 'RIO', city: 'Rio', description: 'Alguma descrição',
+                                  address: 'Endereço', cep: '25000-00', area: 1000)
+  
+        #Act
+        result = warehouse.valid?
+  
+        #Assert
+        expect(result).to eq false
+      end
+  
+      it 'false when code is empty' do
+        #Arrange
+        warehouse = Warehouse.new(name: 'Rio', code: '', city: 'Rio', description: 'Alguma descrição',
+                                  address: 'Endereço', cep: '25000-00', area: 1000)
+  
+        #Act
+        result = warehouse.valid?
+  
+        #Assert
+        expect(result).to eq false
+      end
+  
+      it 'false when city is empty' do
+        #Arrange
+        warehouse = Warehouse.new(name: 'Rio', code: 'RIO', city: '', description: 'Alguma descrição',
+                                  address: 'Endereço', cep: '25000-00', area: 1000)
+  
+        #Act
+        result = warehouse.valid?
+  
+        #Assert
+        expect(result).to eq false
+      end
+    end
+
+    it 'false when code is already in use' do
+      #Arrange
+      first_warehouse = Warehouse.create(name: 'Rio', code: 'RIO', city: 'Rio', description: 'Alguma descrição',
+                                         address: 'Endereço', cep: '25000-00', area: 1000)
+
+      second_warehouse = Warehouse.new(name: 'Niterói', code: 'RIO', city: 'Niterói', description: 'Outra descrição',
+                                       address: 'Avenida', cep: '35000-00', area: 1500)
+
+      #Act
+      result = second_warehouse.valid?
+
+      #Assert
+      expect(result).to eq false
+
+    end
+  end
 end
--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -13,8 +13,10 @@ describe 'Usuário visita tela inicial' do
 
   it 'e vê os galpões cadastrados' do
     #Arrange
-    Warehouse.create(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000)
-    Warehouse.create(name: 'Maceio', code: 'MCZ', city: 'Maceio', area: 50_000)
+    Warehouse.create(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000, 
+                     address: 'Av do Porto, 1000', cep: '2000-000', description: 'Galpão do Rio')
+    Warehouse.create(name: 'Maceio', code: 'MCZ', city: 'Maceio', area: 50_000,
+                     address: 'Av Atlantica, 50', cep: '8000-000', description: 'Perto do aeroporto')
 
     #Act
     visit(root_path)
